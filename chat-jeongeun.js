64d6a6537113063819edd7bf915b5db9bf2fc8f9
jeogeun2 <lovedms7@naver.com>
2016-02-20 12:00:39 +0900

    d

--- chat-jeongeun.js
+++ chat-jeongeun.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.send('./index.html');
+  res.sendfile('./index.html');
 });
 
 http.listen(3000, function(){
